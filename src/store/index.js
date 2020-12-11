c9fe1a71ed64cfcdb2e36026521b862fc4136f48
h-bitstream <1750218633@qq.com>
2020-12-11 21:04:17 +0800

    vuex案例提交

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,85 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    // 所有的任务列表
+    list: [],
+    inputValue: 'aaa',
+    nextId: 5,
+    viewKey: 'all'
   },
   mutations: {
+    initList(state, list) {
+      state.list = list
+    },
+    // 为 store 中的 inputValue 赋值
+    setInputValue(state, val) {
+      state.inputValue = val
+    },
+    // 添加列表项
+    addItem(state) {
+      const obj = {
+        id: state.nextId,
+        info: state.inputValue.trim()
+      }
+      state.list.push(obj)
+      state.nextId++
+      state.inputValue = ''
+    },
+    // 根据id删除对应的任务事项
+    removeItem(state, id) {
+      // 根据id查找对应的索引
+      const i = state.list.findIndex(x => x.id === id)
+      // 根据索引删除对应的元素
+      if (i !== -1) {
+        state.list.splice(i, 1)
+      }
+    },
+    statusChange(state, param) {
+      // 
+      const i = state.list.findIndex(x => x.id === param.id)
+      if (i !== -1) {
+        state.list[i].done = param.status
+      }
+    },
+    cleanDone(state) {
+      state.list = state.list.filter(x => x.done === false)
+    },
+    // 修改视图的关键字
+    changeViewKey(state, key) {
+      state.viewKey = key
+    }
   },
   actions: {
+    getList(context) {
+      axios.get('/list.json').then(({ data }) => {
+        // console.log(data)
+        context.commit('initList', data)
+      })
+    }
   },
   modules: {
+  },
+  getters: {
+    // 统计未完成的任务条数
+    unDoneLength(state) {
+      return state.list.filter(x => x.done === false).length
+    },
+    infoList(state) {
+      if(state.viewKey === 'all') {
+        return state.list
+      }
+      if(state.viewKey === 'undone') {
+        return state.list.filter(x => !x.done)
+      }
+      if(state.viewKey === 'done') {
+        return state.list.filter(x => x.done)
+      }
+      return state.list
+    }
   }
 })
